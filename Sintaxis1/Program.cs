a5bc503232e810fe4e0ec73f7dcc7ea4741b307a
Daniel Jiménez Galván <78568461+DanyJim@users.noreply.github.com>
2021-04-19 16:14:31 -0500

    Sintaxi1_6V
    Sintaxis1_6V

---
+++ Sintaxis1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sintaxis1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (Lenguage l = new Lenguage())
+                {
+                    /*while (!l.FinArchivo())
+                    {
+                        l.NextToken();
+                    }*/
+                    l.match("#");
+                    l.match("include");
+                    l.match("<");
+                    l.match(Token.Clasificaciones.Identificador);
+                    l.match(".");
+                    l.match("h");
+                    l.match(">");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sintaxis1/Sintaxis.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sintaxis1
+{
+    //Requerimiento 1: Agregar en el archivo log la fecha y la hora
+    class Sintaxis:Lexico
+    {
+        public Sintaxis()
+        {
+            NextToken();
+        }
+        public void match(string espera)
+        {
+            if (espera == getContenido())
+            {
+                NextToken();
+            }
+            else
+            {
+                throw new Exception("Error de Sintaxis: Se espera un "+espera);
+            }
+        }
+        public void match(Clasificaciones espera)
+        {
+            if (espera == getClasificacion())
+            {
+                NextToken();
+            }
+            else
+            {
+                throw new Exception("Error de Sintaxis: Se espera un " + espera);
+            }
+        }
+    }
+}
